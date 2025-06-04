03eb47fe013f980921ed6d51ba2a0f21bafafb77
cedric <2361751078@qq.com>
2025-06-04 14:48:38 +0800

    2025-6-4-14:48

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
+import ElementUI from 'element-ui';
 import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false
+Vue.use(ElementUI);
 
 new Vue({
   router,
